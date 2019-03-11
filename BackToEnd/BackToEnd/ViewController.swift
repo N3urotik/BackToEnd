053c7814aca69c9dae7b61d2eb761911621be186
pacificocatapano <pacifico.catapano@gmail.com>
2019-03-11 16:18:52 +0100

    struct

--- BackToEnd/BackToEnd/ViewController.swift
+++ BackToEnd/BackToEnd/ViewController.swift
@@ -7,73 +7,84 @@
 //
 
 import UIKit
-
-struct Aeroporti:Codable {
-    let aeroporti: Aeroporto
+import Foundation
+public struct Airports:Codable {
+    let airports: [Airport]
 }
 
-struct Coordinate:Codable {
-    let longitudine: Double
-    let latitudine: Double
+public struct Coordinate:Codable {
+    let Latitude: Double
+    let Longitude: Double
 }
 
-struct Nome:Codable{
-    let codLinguaggio: String
-    let nomeAeroporto: String
+public struct Name:Codable{
+    let @LanguageCode: String
+    let `$`: String
 }
 
-struct Distanza: Codable{
-    let valore: Float
-    let unità: String
+public struct Distance: Codable{
+    let Value: Float
+    let UOM: String
 }
 
-struct Aeroporto:Codable {
-    let codAereo: String
-    let posizione: Coordinate
-    let codCittà: String
-    let codStato: String
-    let tipoStruttura: String
-    let nomi: Nome
-    let distanze: Distanza
+public struct Airport:Codable {
+    let AirportCode: String
+    let Position: Coordinate
+    let CityCode: String
+    let CountryCode: String
+    let LocationType: String
+    let Names: Name
+    let Distance: Distance
 }
 
 
 
 
-let jsonStringData = "Nearest airport".data(using: .utf8)!
-//let decoder = JSONDecoder()
-let vicino = try JSONDecoder().decode([Aeroporti].self, from: jsonStringData)
-//let vicino = try decoder.decode(Aeroporti.self, from: jsonStringData)
-
-
-
-func AeroportoVicino(){
-    let key = "t6gnvthb46ue64525naf6cbp"
-    var input:Coordinate = Coordinate.init(longitudine: 38.193, latitudine: 15.552)
-    let session = URLSession(configuration: URLSessionConfiguration.default, delegate: nil, delegateQueue: OperationQueue.main)
-    
-//    curl -H "Authorization: Bearer t6gnvthb46ue64525naf6cbp" -H "Accept: application/json"
-    
-    let endpoint = "https://api.lufthansa.com/v1/references/airports/nearest/\(input)?lang=it&appid=\(key)"
-    
-    let safeUrlString = endpoint.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
-    
-    guard let endPointUrl = URL(string: safeUrlString!) else {
-        print("Url non valido")
-        return
-    }
-}
+let jsonStringData = "Nearest Airport".data(using: .utf8)!
+let decoder = JSONDecoder()
+let aeroporti = try decoder.decode(Aeroporti.self, from: jsonStringData)
 
 class ViewController: UIViewController {
-    
-    
 
+    func AeroportoVicino(){
+        
+        //DICHIARAZIONE LINK API CON RELATIVA CHIAVE
+        let session = URLSession(configuration: URLSessionConfiguration.default,
+                                 delegate: nil, delegateQueue: OperationQueue.main)
+        let endpoint = "https://api.lufthansa.com/v1/references/airports/nearest/38.500,11.000"
+        let safeUrlString = endpoint.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+        guard let endpointURL = URL(string: safeUrlString!) else {
+            print("Url non valido")
+            return
+        }
+        let key = "3xwxrgw6f9jtqwy77refs3dd"
+        var request = URLRequest(url: endpointURL)
+        request.httpMethod = "GET"
+        request.addValue("Bearer \(key)", forHTTPHeaderField: "Authorization")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        let dataTask = session.dataTask(with: request) { (data, response, error) in
+            guard let jsonData = data else { }
+            print("Non ho decodificato")
+            return
+            let decoder = JSONDecoder()
+            do {
+                let aeroporti = try decoder.decode(Aeroporti.self, from: jsonData)
+                print("JSON decoded")
+                //USE THE LOADED DATA HERE
+            } catch let error {
+                print(" JSON decoding failed")
+            }
+        }
+        dataTask.resume()
+    }
+    
+    //    @IBOutlet weak var myLbl: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         AeroportoVicino()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    
 }
-
